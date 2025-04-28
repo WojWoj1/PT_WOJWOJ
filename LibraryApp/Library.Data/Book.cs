b7eb3b1f0733316850ab7522f110d539eca45a81
mateuszmrowicki <matimrowicki@wp.pl>
2025-04-28 17:08:46 +0200

    repo update

--- LibraryApp/Library.Data/Book.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Library.Data
-{
-    public class Book
-    {
-        public string ISBN { get; set; }
-        public string Title { get; set; }
-        public bool IsBorrowed { get; set; } = false;
-    }
-}
